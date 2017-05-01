2893dbb902db9ce756450ef5699925362051d4c8
gdomnijl <gdomnijl@gmail.com>
2017-05-01 14:59:42 -0500

    layer 5

--- mock/mock.js
+++ mock/mock.js
@@ -1,5 +1,5 @@
 var svg_width  = 800;
-var svg_height = 600;
+var svg_height = 800;
 
 var svg = d3.select('body').append('svg')
             .attr('width', svg_width)
@@ -17,21 +17,130 @@ var simulation = d3.forceSimulation()
 var colorscale = d3.scaleLinear()
                     .domain([0, 20])
                      .range(["#e5f5f9", "#2ca25f"]);
-                     
+
+//random number generator 
+function randomWholeNum(diff,min) {
+    var randNum = Math.floor(Math.random() * diff) + min
+    console.log(randNum);
+    return randNum;
+}
+
+//MACRO CONSTANTS:
+var NUM_NEIGHBOR = 10;
+
 var nodes = []
 var links = []
-   
-d3.queue()
-.defer(d3.json, 'MOCK_DATA2.json')
-.await(function(error,data){
-d3.json("MOCK_DATA2.json", function(data){
-   for(row of data) {
-        
-        nodes.push({
-            "word": row.Word, "frequency":parseFloat(row.Frequency)});
-       links.push({"source": row.Word, "target":row.Related, "value":parseFloat(row.Similarity)});
-    }
+var indexRecord = []
 
+function indexSoFar() {
+    return indexRecord[indexRecord.length-1];
+}
+
+function addToGraph(i, data, selectIndex){
+    nodes.push({
+        "word": data[i].Word, 
+        "frequency":parseFloat(data[i].Frequency)});
+    links.push({
+         "source": data[selectIndex].Word,
+         "target":data[i].Word, 
+         "value":parseFloat(data[i].Similarity)});
+}
+
+
+
+d3.queue()
+.defer(d3.json, 'MOCK_DATA.json')
+.await(function(error,data){
+    
+    var indexSoFar = 0;
+   // indexRecord.push(0);
+   var numNode1 = NUM_NEIGHBOR; //randomWholeNum(20,1);
+
+    //First layer
+    for(var i = 0; i < numNode1; i++) {
+       
+     nodes.push({
+         "word": data[i].Word, 
+         "frequency":parseFloat(data[i].Frequency)});
+    
+     links.push({
+         "source": "Test", 
+         "target":data[i].Word, 
+         "value":parseFloat(data[i].Similarity)});
+    }
+    
+    indexRecord.push(numNode1);
+    indexSoFar = numNode1;
+    console.log("first layer sofar: "+ indexRecord);
+    
+    nodes.push({
+        "word":"Test",
+        "frequency": 0.65});
+
+    //Second layer from subnodeA:
+    var numNode2A = NUM_NEIGHBOR;
+        console.log("subnodeA:")
+    var subnodeA = randomWholeNum(numNode1,0);
+ 
+    for(var j = indexSoFar; j < indexSoFar+numNode2A;j++) {
+        addToGraph(j, data, subnodeA);
+    }
+    
+    indexRecord.push(indexRecord[indexRecord.length-1] + numNode2A);
+    indexSoFar = indexSoFar + numNode2A;
+    console.log("second layer sofar: "+ indexRecord);
+
+    //Second layer from subnodeB:
+    var numNode2B = NUM_NEIGHBOR;
+    console.log("subnodeB:");
+    var subnodeB = randomWholeNum(numNode1,0);
+    for(var i = indexSoFar; i < indexSoFar + numNode2B; i++) {
+        addToGraph(i, data, subnodeB);
+    }
+        indexRecord.push(indexRecord[indexRecord.length-1] + numNode2B);
+
+    indexSoFar = indexSoFar + numNode2B;
+    console.log("second layer sofar: "+indexRecord);
+
+    //Third layer from subnodeAC: 
+    var numNode3C = NUM_NEIGHBOR;
+    console.log("subnodeC:")
+    var subnodeC = randomWholeNum(numNode2A,numNode1);
+    
+    for(var i = indexSoFar; i < indexSoFar + numNode3C; i++) {
+       addToGraph(i,data,subnodeC);
+    }
+    
+    indexRecord.push(indexRecord[indexRecord.length-1] + numNode3C);
+
+    indexSoFar = indexSoFar + numNode3C;
+    console.log("third layer sofar: "+indexSoFar);
+
+    //Forth layer from subnode ACD:
+      var numNode4D = NUM_NEIGHBOR;
+    console.log("subnodeD:")
+    var subnodeD = randomWholeNum(numNode4D,numNode1+numNode2A);
+    
+    for(var i = indexSoFar; i < indexSoFar + numNode4D; i++) {
+       addToGraph(i,data,subnodeD);
+    }
+    indexRecord.push(indexRecord[indexRecord.length-1] + numNode4D);
+    indexSoFar = indexSoFar + numNode4D;
+    console.log("forth layer sofar: "+ indexRecord);
+
+    
+    //Fifth layer from subnode ACDE:
+      var numNode5E = NUM_NEIGHBOR;
+    console.log("subnodeE:")
+    var subnodeE = randomWholeNum(numNode5E,indexRecord[indexRecord.length-1]);
+    
+    for(var i = indexSoFar; i < indexSoFar + numNode5E; i++) {
+       addToGraph(i,data,subnodeE);
+    }
+    indexRecord.push(indexRecord[indexRecord.length-1] + numNode5E);
+    indexSoFar = indexSoFar + numNode5E;
+    console.log("fifth layer sofar: "+ indexRecord);
+    
     
     var link = svg.append("g")
             .attr("class", "links")
@@ -50,7 +159,9 @@ d3.json("MOCK_DATA2.json", function(data){
                 .attr("id", d => d.word)
     //The more frequent a word is, the bigger it is
                 .attr("r", function(d) {return d.frequency * 5 + 5})
-                .attr("fill", d => colorscale(d.frequency*10 + 5))
+                .attr("fill", function(d){
+                    if(d.word == "Test"){ return "black"; } 
+                    else { return colorscale(d.frequency*10 + 5)}})
                     .call(d3.drag()
                     .on("start", dragstarted)
                     .on("drag", dragged)
