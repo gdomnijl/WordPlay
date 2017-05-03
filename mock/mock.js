407401d959ad8b47897b6b719b9fac625cd85481
An Hoang <An@Ans-MacBook-Pro.local>
2017-05-03 16:28:12 -0500

    finished An version

--- mock/mock.js
+++ mock/mock.js
@@ -1,9 +1,12 @@
 var svg_width  = 800;
-var svg_height = 800;
+var svg_height = 600;
+
+                     
 
 var svg = d3.select('body').append('svg')
             .attr('width', svg_width)
             .attr('height', svg_height)
+            .attr("class", "graph-svg-component");
 
 var manyBody = d3.forceManyBody()
                  //.strength();
@@ -17,168 +20,58 @@ var simulation = d3.forceSimulation()
 var colorscale = d3.scaleLinear()
                     .domain([0, 20])
                      .range(["#e5f5f9", "#2ca25f"]);
-
-//random number generator 
-function randomWholeNum(diff,min) {
-    var randNum = Math.floor(Math.random() * diff) + min
-    console.log(randNum);
-    return randNum;
-}
-
-//MACRO CONSTANTS:
-var NUM_NEIGHBOR = 10;
-
+                     
 var nodes = []
 var links = []
-var indexRecord = []
-
-function indexSoFar() {
-    return indexRecord[indexRecord.length-1];
-}
-
-function addToGraph(i, data, selectIndex){
-    nodes.push({
-        "word": data[i].Word, 
-        "frequency":parseFloat(data[i].Frequency)});
-    links.push({
-         "source": data[selectIndex].Word,
-         "target":data[i].Word, 
-         "value":parseFloat(data[i].Similarity)});
-}
 
 
-
+/// making nodes and link arrays   
 d3.queue()
 .defer(d3.json, 'MOCK_DATA.json')
 .await(function(error,data){
-    
-   var indexSoFar = 0;
-   // indexRecord.push(0);
-   var numNode1 = NUM_NEIGHBOR; //randomWholeNum(20,1);
-
-    //First layer
-    for(var i = 0; i < numNode1; i++) {
-       
-     nodes.push({
-         "word": data[i].Word, 
-         "frequency":parseFloat(data[i].Frequency)});
-    
-     links.push({
-         "source": "Test", 
-         "target":data[i].Word, 
-         "value":parseFloat(data[i].Similarity)});
+  
+    for(var i = 0; i < 20; i++) {
+        
+        nodes.push({//"id": n,
+            "word": data[i].Word, "frequency":parseFloat(data[i].Frequency)});
+       links.push({"source": "Test", "target":data[i].Word, "value":parseFloat(data[i].Similarity)});
     }
-    
-    indexRecord.push(numNode1);
-    indexSoFar = numNode1;
-    console.log("first layer sofar: "+ indexRecord);
-    
-    nodes.push({
-        "word":"Test",
-        "frequency": 0.65});
-
-    //Second layer from subnodeA:
-    var numNode2A = NUM_NEIGHBOR;
-        console.log("subnodeA:")
-    var subnodeA = randomWholeNum(numNode1,0);
- 
-    for(var j = indexSoFar; j < indexSoFar+numNode2A;j++) {
-        addToGraph(j, data, subnodeA);
-    }
-    
-    indexRecord.push(indexRecord[indexRecord.length-1] + numNode2A);
-    indexSoFar = indexSoFar + numNode2A;
-    console.log("second layer sofar: "+ indexRecord);
-
-    //Second layer from subnodeB:
-    var numNode2B = NUM_NEIGHBOR;
-    console.log("subnodeB:");
-    var subnodeB = randomWholeNum(numNode1,0);
-    for(var i = indexSoFar; i < indexSoFar + numNode2B; i++) {
-        addToGraph(i, data, subnodeB);
-    }
-        indexRecord.push(indexRecord[indexRecord.length-1] + numNode2B);
-
-    indexSoFar = indexSoFar + numNode2B;
-    console.log("second layer sofar: "+indexRecord);
-
-    //Third layer from subnodeAC: 
-    var numNode3C = NUM_NEIGHBOR;
-    console.log("subnodeC:")
-    var subnodeC = randomWholeNum(numNode2A,numNode1);
-    
-    for(var i = indexSoFar; i < indexSoFar + numNode3C; i++) {
-       addToGraph(i,data,subnodeC);
-    }
-    
-    indexRecord.push(indexRecord[indexRecord.length-1] + numNode3C);
-
-    indexSoFar = indexSoFar + numNode3C;
-    console.log("third layer sofar: "+indexSoFar);
-
-    //Forth layer from subnode ACD:
-      var numNode4D = NUM_NEIGHBOR;
-    console.log("subnodeD:")
-    var subnodeD = randomWholeNum(numNode4D,numNode1+numNode2A);
-    
-    for(var i = indexSoFar; i < indexSoFar + numNode4D; i++) {
-       addToGraph(i,data,subnodeD);
-    }
-
-
-    indexRecord.push(indexRecord[indexRecord.length-1] + numNode4D);
-    indexSoFar = indexSoFar + numNode4D;
-    console.log("forth layer sofar: "+ indexRecord);
+    nodes.push({"word": "Test", "frequency": 0.6});
 
     
-    //Fifth layer from subnode ACDE:
-    var numNode5E = NUM_NEIGHBOR;
-    console.log("subnodeE:")
-    var subnodeE = randomWholeNum(numNode5E,indexRecord[indexRecord.length-1]);
-    
-    for(var i = indexSoFar; i < indexSoFar + numNode5E; i++) {
-       addToGraph(i,data,subnodeE);
-    }
-    indexRecord.push(indexRecord[indexRecord.length-1] + numNode5E);
-    indexSoFar = indexSoFar + numNode5E;
-    console.log("fifth layer sofar: "+ indexRecord);
-    
-    
+//fill in links
     var link = svg.append("g")
-            .attr("class", "links")
             .selectAll("line")
             .data(links)
             .enter().append("line")
+            .attr("class", "links")
     //The more similar the words are, the thicker the links
             .attr("stroke-width", d=> 5*d.value + 1)
             .attr("stroke", d => colorscale(d.value*10 +5)); 
 
-    var node = svg.append("g")
-                .attr("class", "nodes")
-                .selectAll("circle")
-                .data(nodes)
-                .enter().append("circle")
-                .attr("id", d => d.word)
-    //The more frequent a word is, the bigger it is
-                .attr("r", function(d) {return d.frequency * 5 + 5})
-                .attr("fill", function(d){
-                    if(d.word == "Test"){ return "black"; } 
-                    else { return colorscale(d.frequency*10 + 5)}})
-                    .call(d3.drag()
+//fill in nodes
+var node = svg.selectAll(".node")
+              .data(nodes)
+              .enter().append("g")
+              .attr("class","nodes")
+              .append("circle")
+              .attr("r", function(d) {return d.frequency * 5 + 5})
+    .attr("fill", d => colorscale(d.frequency*10 + 5))
+              .call(d3.drag()
                     .on("start", dragstarted)
                     .on("drag", dragged)
                     .on("end", dragended))
-                    .on("click",connectedNodes)
-                    .on("dblclick.zoom", function(d) { d3.event.stopPropagation();
-	var dcx = (window.innerWidth/2-d.x*zoom.scale());
-	var dcy = (window.innerHeight/2-d.y*zoom.scale());
-	zoom.translate([dcx,dcy]);
-	 g.attr("transform", "translate("+ dcx + "," + dcy  + ")scale(" + zoom.scale() + ")");
-	 
-	 
-	});
-	
-    
+                    .on("click",connectedNodes);
+
+//fill in labels
+text = svg.selectAll(".nodes")
+    .data(nodes)
+    .append("text")
+    .attr("dx",10)
+    .attr("dy", ".35em")
+    .text(function(d) { return d.word})
+
+
     simulation.nodes(nodes)
     .on("tick", ticked);
 
@@ -198,9 +91,19 @@ d3.queue()
     node
       .attr("cx", d => d.x )
       .attr("cy", d => d.y );
+///// make the text moves with the node
+    d3.selectAll("text").attr("x", function (d) {
+        return d.x;
+    })
+        .attr("y", function (d) {
+        return d.y;
+    });
+
   }
-    
-    //---Insert-------
+
+  
+/////////// click to highlight function
+//---Insert-------
 
 //Toggle stores whether the highlighting is on
 var toggle = 0;
@@ -231,6 +134,10 @@ function connectedNodes() {
         link.style("opacity", function (o) {
             return d.index==o.source.index | d.index==o.target.index ? 1 : 0.1;
         });
+
+        text.style("opacity", function (o) {
+            return neighboring(d, o) | neighboring(o, d) ? 1 : 0.1;
+        });
         
         //Reduce the op
         
@@ -239,11 +146,13 @@ function connectedNodes() {
         //Put them back to opacity=1
         node.style("opacity", 1);
         link.style("opacity", 1);
+        text.style("opacity", 1);
         toggle = 0;
     }
 
 }
-})
+
+//---End Insert---
 
 function dragstarted(d) {
   if (!d3.event.active) simulation.alphaTarget(0.3).restart();
@@ -261,4 +170,58 @@ function dragended(d) {
   d.fx = null;
   d.fy = null;
 }
+
+
+////////Searchbox
+var optArray = [];
+for (var i = 0; i < nodes.length - 1; i++) {
+    optArray.push(nodes[i].word);
+}
+optArray = optArray.sort();
+console.log("optArray");
+console.log(optArray);
+
+$(function () {
+    $("#search2").select2({
+  data: optArray,
+  //placeholder: "Select a state",
+  //allowClear: true
+});
+    
+});
+
+
+
+
+
+
+});
+
+
+
+
+function searchNode() {
+    //find the node
+    var selectedVal = document.getElementById('search2').value;
+    var node = svg.selectAll(".nodes");
+    if (selectedVal == "none") {
+        node.style("stroke", "white").style("stroke-width", "1");
+    } else {
+        var selected = node.filter(function (d, i) {
+            return d.word != selectedVal;
+        });
+        selected.style("opacity", "0");
+        var link = svg.selectAll(".links")
+        link.style("opacity", "0");
+        d3.selectAll(".nodes, .links").transition()
+            .duration(5000)
+            .style("opacity", 1);
+    }
+}
+//// end search box
+
+
+
+
+
 
