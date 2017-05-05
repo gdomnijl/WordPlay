a46d6acb39cef9523bd66ced366889851e418bab
Tran Ngoc Phuong Minh <minhntran@Minhs-MacBook-Air.local>
2017-05-05 16:00:56 -0500

    fixed server codes

--- mock/pyserver.py
+++ mock/pyserver.py
@@ -14,12 +14,14 @@ class myHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
 		if self.path.startswith("/"):
 			self.do_html()
-		#elif self.path.startswith("/css"):
-			#self.do_css()
-		#elif self.path.startswith("/mock_single_word.js"):
-			#self.do_js()
-		#elif self.path == "/server":
-		#	self.do_server()
+		elif self.path == "/css/select2.min.css":
+			self.do_css()
+		elif self.path == "/js/select2.min.js":
+			self.do_j()
+		elif self.path == "/mock_single_word.js":
+			self.do_js()
+		elif self.path == '/maddie.json':
+			self.do_file()
 		elif self.path.startswith("/model/"):
 			inputval = self.path[len('/model/'):]
 			self.do_model(inputval)
@@ -34,48 +36,62 @@ class myHandler(BaseHTTPRequestHandler):
 
 		#global model 
 		#vocabulary = model.wv.vocab #USE THIS FOR DROPDOWN!!!
+		
 		code = open('/Users/minhntran/Documents/csc395/WordPlay/mock/index.html')
 		for line in code:
 			self.wfile.write(line)
 		code.close()
-		with open('/Users/minhntran/Documents/csc395/WordPlay/mock/red.json') as data_file:
-			data = json.load(data_file)
-		pprint(data)
-
-		open('/Users/minhntran/Documents/csc395/WordPlay/mock/mock_single_word.js')
 
 
 
 #Serving css file
-	# def do_css(self):
-	# 	self.send_response(200)
-	# 	self.send_header('Content-type', 'text/html')
-	# 	self.end_headers()
+	def do_css(self):
+		self.send_response(200)
+		self.send_header('Content-type', 'text/css')
+		self.end_headers()
 
-	# 	code = open('lab.css')
-	# 	for line in code:
-	# 		self.wfile.write(line)
-	# 	code.close()
+		code = open('/Users/minhntran/Documents/csc395/WordPlay/mock/select2.min.css')
+		for line in code:
+			self.wfile.write(line)
+		code.close()
+
+#Serving select2.min.js file
+	def do_j(self):
+		self.send_response(200)
+		self.send_header('Content-type', 'text/javascript')
+		self.end_headers()
+
+		code = open('/Users/minhntran/Documents/csc395/WordPlay/mock/select2.min.js')
+		for line in code:
+			self.wfile.write(line)
+		code.close()
+
+#Serving json file
+	def do_file(self):
+		self.send_response(200)
+		self.send_header('Content-type', 'application/json')
+		self.end_headers()
+
+		code = open('/Users/minhntran/Documents/csc395/WordPlay/mock/maddie.json')
+		for line in code:
+			self.wfile.write(line)
+		code.close()
+
+
+
+
 #Serving js file
-	# def do_js(self):
-	# 	self.send_response(200)
-	# 	self.send_header('Content-type', 'text/javascript')
-	# 	self.end_headers()
+	def do_js(self):
+		self.send_response(200)
+		self.send_header('Content-type', 'text/javascript')
+		self.end_headers()
+        
+		code = open('/Users/minhntran/Documents/csc395/WordPlay/mock/mock_single_word.js')
+		for line in code:
+			self.wfile.write(line)
+		code.close()
 
-	# 	code = open('/Users/minhntran/Documents/csc395/WordPlay/mock/mock_single_word.js')
-		# for line in code:
-		# 	self.wfile.write(line)
-		# code.close()
-#Serving the server file
-	# def do_server(self):
-	# 	self.send_response(200)
-	# 	self.send_header('Content-type', 'text/html')
-	# 	self.end_headers()
 
-	# 	code = open('wpserver.py')
-	# 	for line in code:
-	# 		self.wfile.write(line)
-	# 	code.close()
 #Serving model
 	def do_model(self,inputval):
 		self.send_response(200)
